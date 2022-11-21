72df616af841be1bc2902e929ae217852c228599
AdamPennIV <101908045+AdamPennIV@users.noreply.github.com>
2022-11-21 17:44:00 -0500

    4InARow App

--- app/src/main/java/com/zybooks/a4inarow/MainActivity.java
+++ app/src/main/java/com/zybooks/a4inarow/MainActivity.java
@@ -2,23 +2,39 @@ package com.zybooks.a4inarow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private NewGame mGame;
+        private NewGame mGame; // establish mGame variable
+        private Button button; // establish first button
 
-    public void newGameCallback(View view) {
-        // button was clicked
-    }
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openNewGame();
+            }
+        });
+    }
+
+
+    public void openNewGame() {
+        Intent intent = new Intent(this, NewGame.class);
+        startActivity(intent);
+        // code to open 2nd activity
     }
 
 
--- app/src/main/java/com/zybooks/a4inarow/NewGame.java
+++ app/src/main/java/com/zybooks/a4inarow/NewGame.java
@@ -4,9 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Random;
+
 public class NewGame extends AppCompatActivity {
     public static final int GRID_SIZE = 7;
-
 
 
 
@@ -14,5 +15,9 @@ public class NewGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
+    }
+
+    public void newGame() {
+
     }
 }
