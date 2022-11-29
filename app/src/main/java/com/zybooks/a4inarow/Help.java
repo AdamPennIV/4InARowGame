299d2f177498a9efc9f285bf8b7a955ccf64bd9b
Sylvan <109154007+UFGators14@users.noreply.github.com>
2022-11-29 10:38:48 -0500

    added help/instructions

---
+++ app/src/main/java/com/zybooks/a4inarow/Help.java
@@ -0,0 +1,16 @@
+package com.zybooks.a4inarow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class Help extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions);
+    }
+}
